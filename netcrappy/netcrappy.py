5bd6236d4204b291da196685a06939d4bc85374b
Jesse Evers <jesse.e.evers@lmco.com>
2014-07-18 17:52:39 -0400

    created method in Cluster to handle the *-get-iter api

--- netcrappy/netcrappy.py
+++ netcrappy/netcrappy.py
@@ -286,49 +286,75 @@ class Cluster(Filer):
         """
         self.conn.set_vserver(vserver)
 
+    def api_get_iter(self,  iter_api):
+        """@todo: Docstring for api_get_iter.
+
+        :iter_api: @todo
+        :returns: @todo
+
+        """
+        obj_list = []
+        objs = self.invoke(iter_api)
+        obj_list = obj_list + objs.child_get('attributes-list').children_get()
+        next_tag = objs.child_get_string('next-tag')
+        while next_tag is not None:
+            objs = self.invoke(iter_api, 'tag', next_tag)
+            obj_list = obj_list + objs.child_get('attributes-list').children_get()
+            next_tag = objs.child_get_string('next-tag')
+        return obj_list
+
     def get_vservers(self):
         """@todo: Docstring for get_vservers.
         :returns: @todo
 
         """
-
-        vserver_list = []
-        vservers = self.invoke('vserver-get-iter')
-        vserver_list = vserver_list + vservers.child_get('attributes-list').children_get()
-        next_tag = vservers.child_get_string('next-tag')
-        while next_tag is not None:
-            vservers = self.invoke('vserver-get-iter', 'tag', next_tag)
-            vserver_list = vserver_list + vservers.child_get('attributes-list').children_get()
-            next_tag = vservers.child_get_string('next-tag')
+        vserver_list = self.api_get_iter('vserver-get-iter')
         vserver_dict = {}
         for vserver in vserver_list:
             name = vserver.child_get_string('vserver-name')
             state = vserver.child_get_string('state')
             type = vserver.child_get_string('vserver-type')
-            #allowed_protocols = [proto.child_get_string('protocol') for proto in vserver.child_get('allowed-protocols').children_get() if vserver.child_get('allowed-protocols') is not None]
+            if vserver.child_get('allowed-protocols') is not None:
+                allowed_protocols = []
+                for proto in vserver.child_get('allowed-protocols').children_get():
+                    #it should work like this:
+                    #allowed_protocols.append(proto.child_get_string('protocol'))
+                    #but NOOOOOOOOOOOOOOOO
+                    inside_xml = re.compile('<protocol>(.*)</protocol>')
+                    allowed_protocols.append(inside_xml.findall(proto.sprintf())[0])
+            else:
+                allowed_protocols = None
+            if vserver.child_get('vserver-aggr-info-list') is not None:
+                #This will only return data if an aggr has been delegated 
+                #to the vserver
+                aggr_dict = {}
+                for aggr in vserver.child_get('vserver-aggr-info-list').children_get():
+                    aggr_name = aggr.child_get_string('aggr-name')
+                    aggr_avail = aggr.child_get_int('aggr-availsize')
+                    aggr_dict[aggr_name] = {'aggr-availsize': aggr_avail}
+            else:
+                aggr_dict = None
+
             vserver_dict[name] = {'state': state,
                                   'type': type,
-                                  #'allowed-protocols': allowed_protocols
+                                  'allowed-protocols': allowed_protocols,
+                                  'vserver-aggr-info': aggr_dict
                                  }
         return vserver_dict
 
-    def get_volumes(self,  vserver='', max_records=20):
+    def get_volumes(self, vserver=None, max_records=20):
         """@todo: Docstring for get_volumes.
 
         :vserver: @todo
         :returns: @todo
 
         """
-        self.set_vserver(vserver)
-        volume_list = []
-        volumes = self.invoke('volume-get-iter')
-        volume_list = volume_list + volumes.child_get('attributes-list').children_get()
-        next_tag = volumes.child_get_string('next-tag')
-        while next_tag is not None:
-            volumes = self.invoke('volume-get-iter', 'tag', next_tag)
-            volume_list = volume_list + volumes.child_get('attributes-list').children_get()
-            next_tag = volumes.child_get_string('next-tag')
-        self.set_vserver('')
+        if vserver:
+            orig_vserver = self.conn.get_vserver()
+            self.set_vserver(vserver)
+        volume_list = self.api_get_iter('volume-get-iter')
+        if vserver:
+            self.set_vserver(orig_vserver)
         volumes_dict = {}
         for volume in volume_list:
             name = volume.child_get('volume-id-attributes').child_get_string('name')
@@ -338,6 +364,17 @@ class Cluster(Filer):
                                   'owning-vserver-name': owning_vserver
                                  }
         return volumes_dict
+
+    def create_vol(self, name, aggr, size):
+        """@todo: Docstring for create_vol.
+
+        :name: @todo
+        :aggr: @todo
+        :size: @todo
+        :returns: @todo
+
+        """
+        pass
         
 
 
