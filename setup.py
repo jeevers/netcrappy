520406103631dc0fdd670e2eb3ea9cea9e01844f
Jesse Evers <jeevers@jesseeevers.com>
2015-02-17 09:23:43 -0500

    initial setup.py script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='netcrappy',
+      version='1.0',
+      description='Wrapper for the NetApp Manageability SDK (NMSDK)',
+      author='Jesse Evers',
+      author_email='jeevers@jesseeevers.com',
+      url='https://github.com/jeevers/netcrappy',
+      packages=['netcrappy']
+     )
